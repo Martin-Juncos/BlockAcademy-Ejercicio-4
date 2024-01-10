14aa0b7f8ee4bca088e10e1768e5f58360977281
Carlos Martin Juncos <prof.mcjuncos@gmail.com>
2024-01-09 22:15:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// ¿Cuál crees que será el resultado de la ejecución de estas operaciones?:
+
+console.log("1: ", 6 / "3"); //
+console.log("2: ", "2" * "3"); //
+console.log("3: ", 4 + 5 + "px"); //
+console.log("4: ", "$" + 4 + 5); //
+console.log("5: ", "4" - 2); //
+console.log("6: ", "4px" - 2); //
+console.log("7: ", 7 / 0); //
+console.log("8: ", parseInt("09")); //
+console.log("9: ", 5 && 2); //
+console.log("10: ", 2 && 5); //
+console.log("11: ", 5 || 0); //
+console.log("12: ", 0 || 5); //
+console.log("13: ", [3] + [3] - [10]); //
+console.log("14: ", 3 > 2 > 1); //
